a9a49b3f8ac14f720c55a7831b7d087c7008362b
Benjamin Höglinger-Stelzer <nefarius@dhmx.at>
2024-08-08 17:59:07 +0200

    Works

--- ports/neflib/portfile.cmake
+++ ports/neflib/portfile.cmake
@@ -1,54 +1,31 @@
+if(NOT VCPKG_TARGET_IS_WINDOWS)
+    vcpkg_check_linkage(ONLY_STATIC_LIBRARY)
+endif()
+
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO nefarius/neflib
     REF master
-    SHA512 0
+    SHA512 5ec0cf5387ffc54783fa5a071fd579e100dcbf368ff281f2a4f7bf0bf83be8e984466f178bd4688f51154588e7370f6491f4dd09b9b2e47dd203f0f005e10852
 )
 
-vcpkg_cmake_configure(
-    SOURCE_PATH ${SOURCE_PATH}
-    PREFER_NINJA
+vcpkg_check_linkage(ONLY_STATIC_LIBRARY)
+
+vcpkg_install_msbuild(
+    SOURCE_PATH "${SOURCE_PATH}"
+    PROJECT_SUBPATH src/neflib.vcxproj
+    RELEASE_CONFIGURATION "Release"
+    DEBUG_CONFIGURATION "Debug"
+    PLATFORM ${PLATFORM}
 )
 
-vcpkg_install_cmake()
-
-file(INSTALL ${SOURCE_PATH}/include DESTINATION ${CURRENT_PACKAGES_DIR})
+vcpkg_fixup_pkgconfig()
 
 vcpkg_copy_pdbs()
 
-file(WRITE ${CURRENT_PACKAGES_DIR}/share/neflib/vcpkg-cmake-wrapper.cmake
-"
-# This file is a wrapper for configuring the CMake project that consumes this package.
-# Add any additional variables or configuration options you want to automatically
-# apply when this port is consumed.
+file(COPY
+    "${SOURCE_PATH}/include/nefarius/neflib"
+    DESTINATION "${CURRENT_PACKAGES_DIR}/include/nefarius/neflib"
+    FILES_MATCHING PATTERN *.h PATTERN *.hpp)
 
-set(neflib_DIR \${CURRENT_PACKAGES_DIR}/lib/cmake/neflib CACHE STRING \"neflib library path\")
-")
-
-vcpkg_cmake_configure(
-    SOURCE_PATH ${SOURCE_PATH}
-    PREFER_NINJA
-    OPTIONS
-        -DCMAKE_TOOLCHAIN_FILE=${VCPKG_CMAKE_TOOLCHAIN_FILE}
-        -DVCPKG_TARGET_TRIPLET=${TARGET_TRIPLET}
-)
-
-vcpkg_fixup_cmake_targets(CONFIG_PATH lib/cmake/neflib)
-vcpkg_copy_pdbs()
-vcpkg_install_cmake()
-
-file(INSTALL ${SOURCE_PATH}/include DESTINATION ${CURRENT_PACKAGES_DIR}/include)
-file(INSTALL ${SOURCE_PATH}/src DESTINATION ${CURRENT_PACKAGES_DIR}/src)
-
-vcpkg_copy_pdbs()
-
-file(WRITE ${CURRENT_PACKAGES_DIR}/share/neflib/vcpkg-cmake-wrapper.cmake
-"
-# This file is a wrapper for configuring the CMake project that consumes this package.
-# Add any additional variables or configuration options you want to automatically
-# apply when this port is consumed.
-
-set(neflib_DIR \${CURRENT_PACKAGES_DIR}/lib/cmake/neflib CACHE STRING \"neflib library path\")
-")
-
-vcpkg_fixup_cmake_targets(CONFIG_PATH lib/cmake/neflib)
+vcpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/LICENSE")
